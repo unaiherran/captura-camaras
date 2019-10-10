228e514f1f79b9835d9b1634bfddceaa7eaf4edd
Unai Herran <unai.herran@gmail.com>
2019-10-11 01:13:44 +0200

    Posibilidad de añadir parametros 😕

--- scrap_camara.py
+++ scrap_camara.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+import sys, getopt
+import argparse
+
 import requests
 import settings
 from PIL import Image
@@ -12,12 +17,14 @@ import logging
 
 def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
     now = datetime.now()
-
     fecha_str = str(now.year) + str(now.month) + str(now.day) + '_' + str(now.hour) + str(now.minute)
 
     output_list = []
 
     logging.basicConfig(filename='crawler.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+    errores = 0
+    adquiridos = 0
 
     # For each camera
     for camera in lista_camaras:
@@ -35,11 +42,13 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
             if response.status_code >= 500:  # Server error
                 # cambiar el estatus de la camara
                 status += 100
+                errores += 1
                 logging.error('Camera: %s Feed %s da un error %s. Nuevo Status: %s', camera[0], camera[1],
                               response.status_code, status)
 
             elif response.status_code >= 400:  # Client Error
                 status += 10
+                errores += 1
                 logging.error('Camera: %s Feed %s da un error %s.  Nuevo Status: %s', camera[0], camera[1],
                               response.status_code, status)
 
@@ -49,10 +58,12 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
                 try:
                     img = Image.open(BytesIO(response.content))
                     # resize file
-                    img = img.resize((settings.OUTPUT_RESOLUTION_WIDTH, settings.OUTPUT_RESOLUTION_HEIGHT),
-                                     Image.ANTIALIAS)
+                    # Probablemente se tenga que hacer en otro proceso
+                    #img = img.resize((settings.OUTPUT_RESOLUTION_WIDTH, settings.OUTPUT_RESOLUTION_HEIGHT),
+                    #                 Image.ANTIALIAS)
                     # save image
                     img.save(name, settings.IMG_OUTPUT_FORMAT)
+                    adquiridos += 1
 
                     if status > 0:
                         status = status - 1
@@ -61,6 +72,7 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
 
                 except IOError:
                     status += 1  # archivo no es imagen
+                    errores += 1
                     logging.error('Camera: %s Feed %s, no es una imagen válida. Nuevo Status: %s', camera[0], camera[1], status)
 
         e = [camera[0], camera[1], camera[2], camera[3], camera[4], str(status)]
@@ -69,31 +81,108 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
 
     # TODO grabar archivo para la proxima vez que hagas el crawl
 
-    return output_list
+    return output_list, errores, adquiridos
 
 
-def leer_listado_camaras():
-    file_name = "lista_camaras.csv"
+def leer_listado_camaras(file_name="lista_camaras.csv"):
 
     with open(file_name, newline='') as file:
         reader = csv.reader(file)
         l = list(map(tuple, reader))
     return l
 
+
 def scrap_video():
+    # TODO De momento no hay nada, si sobra tiempo puedo intentar hacerlo
+
     pass
 
-def actualizar_listado_camaras(l):
-    file_name = "lista_camaras1.csv"
 
+def actualizar_listado_camaras(l, file_name = "lista_camaras1.csv"):
     with open(file_name, 'w', newline='\n') as myfile:
         wr = csv.writer(myfile, lineterminator='\n')
-        wr.writerow(l)
+        for e in l:
+            wr.writerow(e)
+
+
+def main():
+    logging.basicConfig(filename='scrap_camara.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+    descripcion = """Esta es la ayuda del script"""
+
+    parser = argparse.ArgumentParser(description=descripcion)
+    parser.add_argument("-V", "--version", help="show program version", action="store_true")
+    parser.add_argument("-I", "--input", help="CSV file with list of cameras")
+    parser.add_argument("-O", "--output", help="Output CSV file with list of cameras with updated success level")
+    parser.add_argument("-s", "--status_requested", help="status requested (0, las que responden siempre)")
+    parser.add_argument("-v", "--verbose", help="", action="store_true")
+
+    # read arguments from the command line
+    args = parser.parse_args()
+
+    # check for --version or -V
+    if args.version:
+        print("Scrap Camaras de Open Data Madrid, version", settings.version)
+
+    if args.input:
+        inputfile = args.input
+        #todo comprobar que existe el fichero
+
+    else:
+        inputfile = 'lista_camaras.csv'
+
+    if args.output:
+        outputfile = args.output
+    elif args.input:
+        outputfile = inputfile
+    else:
+        outputfile = 'lista_camaras_test_out.csv'
+
+    if args.status_requested:
+        status_requested = int(args.status_requested)
+    else:
+        status_requested = 0
+
+    if args.verbose:
+        verbose = True
+    else:
+        verbose = False
+
+    inicio = datetime.now()
+
+    if verbose:
+        print('Input file is', inputfile)
+        print('Output file is', outputfile)
+        print('status requested is', status_requested)
+        print('Inicio de la operación', inicio)
+
+    lista_camara = leer_listado_camaras(inputfile)
+
+    # lanza el crawler y devuelve una lista actualizada
+    lista_camara_actualizada, errores, adquiridos = crawl(lista_camaras=lista_camara, status_req=status_requested)
+
+    resultado_crawl = "De %s pedidos, se han adquirido %s imagenes, con %s errores" %(len(lista_camara), adquiridos, errores)
+
+    if verbose:
+        print(resultado_crawl)
+
+    if errores:
+        logging.warning(resultado_crawl)
+    else:
+        logging.info(resultado_crawl)
+
+    # escribe la lista actualizada para el siguiente crawl
+    actualizar_listado_camaras(lista_camara_actualizada, file_name=outputfile)
+
+    fin = datetime.now()
+    duracion = fin - inicio
+
+    logging.info('Me ha costado hacer el crawl %s' %(str(duracion)))
+    if verbose:
+        print('Fin de la operación',fin)
+        print('Duracion', duracion)
 
 
 if __name__ == '__main__':
-    print(datetime.now())
-    l = leer_listado_camaras()
-    otra = crawl(l)
-    actualizar_listado_camaras(otra)
-    print(datetime.now())
+    #main(sys.argv[1:])
+    main()
--- settings.py
+++ settings.py
@@ -3,6 +3,8 @@ lista_camaras = [['0001', 'http://informo.munimadrid.es/cameras/Camara06303.jpg'
                 ['0003', 'http://informo.munimadrid.es/cameras/Camara06305.jpg', 'S', '45.19','41.32', '0'],
                 ['0004', 'http://informo.munimadrid.es/cameras/Camara06306.jpg', 'S', '45.19','41.32', '0']]
 
+version = "0.1"
+
 # Variables para el crawler
 headers = {'User-Agent': 'Crawler para Proyecto Estudiante Big Data - Unai Herran'}
 
