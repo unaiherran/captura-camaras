ce78705d953eecce8707e9f9671018850c8e1416
unaiherran <unai.herran@gmail.com>
2019-10-13 13:13:10 +0200

    Añadir multiples loggers y verboses

--- scrap_camara.py
+++ scrap_camara.py
@@ -14,21 +14,40 @@ import csv
 
 import logging
 
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 
-def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """Function setup as many loggers as you want"""
+
+    handler = logging.FileHandler(log_file)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+
+def crawl(lista_camaras = settings.lista_camaras, status_req = 0, verbose = False):
     now = datetime.now()
     fecha_str = str(now.year) + str(now.month) + str(now.day) + '_' + str(now.hour) + str(now.minute)
 
     output_list = []
 
-    logging.basicConfig(filename='crawler.log', level=logging.INFO, format='%(asctime)s %(message)s')
+    logger = setup_logger('crawler_log', 'crawler.log')
 
     errores = 0
     adquiridos = 0
 
+    total_de_camaras = len(lista_camaras)
+    i = 0
+
     # For each camera
     for camera in lista_camaras:
-
+        i += 1
         address = camera[1]
         status = int(camera[5])
         name = settings.OUTPUT_DIR + camera[0] + '_' + fecha_str + '.jpg'
@@ -36,6 +55,8 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
         if status <= status_req:
             # conectarte a la url
             response = requests.get(address, settings.headers)
+            if verbose:
+                print('Leyendo Feed (%s/%s): %s' %(i, total_de_camaras,address))
 
             # gestion de errores HTTP
 
@@ -43,13 +64,19 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
                 # cambiar el estatus de la camara
                 status += 100
                 errores += 1
-                logging.error('Camera: %s Feed %s da un error %s. Nuevo Status: %s', camera[0], camera[1],
+                logger.error('Camera: %s Feed %s da un error %s. Nuevo Status: %s', camera[0], camera[1],
+                              response.status_code, status)
+                if verbose:
+                    print('ERROR: Camera: %s Feed %s da un error %s. Nuevo Status: %s', camera[0], camera[1],
                               response.status_code, status)
 
             elif response.status_code >= 400:  # Client Error
                 status += 10
                 errores += 1
-                logging.error('Camera: %s Feed %s da un error %s.  Nuevo Status: %s', camera[0], camera[1],
+                logger.error('Camera: %s Feed %s da un error %s.  Nuevo Status: %s', camera[0], camera[1],
+                              response.status_code, status)
+                if verbose:
+                    print('ERROR: Camera: %s Feed %s da un error %s. Nuevo Status: %s', camera[0], camera[1],
                               response.status_code, status)
 
             elif response.status_code == 200:
@@ -68,23 +95,27 @@ def crawl(lista_camaras = settings.lista_camaras, status_req = 0):
                     if status > 0:
                         status = status - 1
 
-                    logging.info('Camera: %s Feed %s graba la imagen Nuevo Status: %s', camera[0], camera[1], status)
+                    logger.info('Camera: %s Feed %s graba la imagen Nuevo Status: %s', camera[0], camera[1], status)
 
                 except IOError:
                     status += 1  # archivo no es imagen
                     errores += 1
-                    logging.error('Camera: %s Feed %s, no es una imagen válida. Nuevo Status: %s', camera[0], camera[1], status)
+                    logger.error('Camera: %s Feed %s, no es una imagen válida. Nuevo Status: %s',
+                                 camera[0], camera[1], status)
+                    if verbose:
+                        print('ERROR: Camera: %s Feed %s, no es una imagen válida. Nuevo Status: %s',
+                                 camera[0], camera[1], status)
 
         e = [camera[0], camera[1], camera[2], camera[3], camera[4], str(status)]
 
         output_list.append(e)
 
-    # TODO grabar archivo para la proxima vez que hagas el crawl
-
     return output_list, errores, adquiridos
 
 
 def leer_listado_camaras(file_name="lista_camaras.csv"):
+    """ Lee el listado de camaras de un archivo csv. si no tines este listado se obtiene despues de ejecutar
+    sacar_listado.py"""
 
     with open(file_name, newline='') as file:
         reader = csv.reader(file)
@@ -94,11 +125,13 @@ def leer_listado_camaras(file_name="lista_camaras.csv"):
 
 def scrap_video():
     # TODO De momento no hay nada, si sobra tiempo puedo intentar hacerlo
-
     pass
 
 
 def actualizar_listado_camaras(l, file_name = "lista_camaras1.csv"):
+    """Actualiza el csv del listado de camaras para el siguiente crawl. Graba el archivo segun el nombre que le pases
+    en el parametro file_name"""
+
     with open(file_name, 'w', newline='\n') as myfile:
         wr = csv.writer(myfile, lineterminator='\n')
         for e in l:
@@ -106,9 +139,9 @@ def actualizar_listado_camaras(l, file_name = "lista_camaras1.csv"):
 
 
 def main():
-    logging.basicConfig(filename='scrap_camara.log', level=logging.INFO, format='%(asctime)s %(message)s')
-
     descripcion = """Esta es la ayuda del script"""
+
+    scrap_logger = setup_logger('scrap_camara_log', 'scrap_camera.log')
 
     parser = argparse.ArgumentParser(description=descripcion)
     parser.add_argument("-V", "--version", help="show program version", action="store_true")
@@ -150,26 +183,32 @@ def main():
 
     inicio = datetime.now()
 
+    scrap_logger.info('Input file is: %s' %(inputfile))
+    scrap_logger.info('Output file is %s' %(outputfile))
+    scrap_logger.info('Status requested is %s' %(status_requested))
+    scrap_logger.info('Inicio del scrap %s' %(inicio))
     if verbose:
         print('Input file is', inputfile)
         print('Output file is', outputfile)
         print('status requested is', status_requested)
-        print('Inicio de la operación', inicio)
+        print('Inicio del scrap', inicio)
 
     lista_camara = leer_listado_camaras(inputfile)
 
     # lanza el crawler y devuelve una lista actualizada
-    lista_camara_actualizada, errores, adquiridos = crawl(lista_camaras=lista_camara, status_req=status_requested)
+    lista_camara_actualizada, errores, adquiridos = crawl(lista_camaras=lista_camara, status_req=status_requested,
+                                                          verbose=verbose)
 
-    resultado_crawl = "De %s pedidos, se han adquirido %s imagenes, con %s errores" %(len(lista_camara), adquiridos, errores)
+    resultado_crawl = "De %s pedidos, se han adquirido %s imagenes, con %s errores" %(len(lista_camara), adquiridos,
+                                                                                      errores)
 
     if verbose:
         print(resultado_crawl)
 
     if errores:
-        logging.warning(resultado_crawl)
+        scrap_logger.warning(resultado_crawl)
     else:
-        logging.info(resultado_crawl)
+        scrap_logger.info(resultado_crawl)
 
     # escribe la lista actualizada para el siguiente crawl
     actualizar_listado_camaras(lista_camara_actualizada, file_name=outputfile)
@@ -177,10 +216,11 @@ def main():
     fin = datetime.now()
     duracion = fin - inicio
 
-    logging.info('Me ha costado hacer el crawl %s' %(str(duracion)))
+    scrap_logger.info('Fin del Scrap %s' %(fin))
+    scrap_logger.info('INFO: Me ha costado hacer el crawl %s' %(str(duracion)))
     if verbose:
         print('Fin de la operación',fin)
-        print('Duracion', duracion)
+        print('Duración', duracion)
 
 
 if __name__ == '__main__':
