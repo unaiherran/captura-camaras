8942a5a60e27ccf13970fa9bd5d6e085728c728f
unaiherran <unai.herran@gmail.com>
2019-10-17 00:06:04 +0200

    versiones

--- settings.py
+++ settings.py
@@ -3,12 +3,11 @@ lista_camaras = [['0001', 'http://informo.munimadrid.es/cameras/Camara06303.jpg'
                 ['0003', 'http://informo.munimadrid.es/cameras/Camara06305.jpg', 'S', '45.19','41.32', '0'],
                 ['0004', 'http://informo.munimadrid.es/cameras/Camara06306.jpg', 'S', '45.19','41.32', '0']]
 
-version_scrap = "0.2"
-version_a_s3 = "0.1"
-version_deleter = "0.1"
+version_scrap = "0.3"
+version_a_s3 = "0.2"
 
 # Variables para el crawler
-headers = {'User-Agent': 'Crawler para Proyecto Estudiante Big Data - Unai Herran'}
+headers = {'User-Agent': 'Crawler para Proyecto Estudiante Big Data - unai.herran@gmail.com'}
 
 SCRAP_DIR = 'scrapped/'
 PROCCESED_DIR = 'procesed/'
