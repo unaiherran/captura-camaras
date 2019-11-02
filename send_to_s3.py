357991841b372ed7734bb34021ad3f98195e1000
Unai Herran <unai.herran@gmail.com>
2019-11-02 10:51:00 +0100

    mover el log de to_s3.log una vez al dia a s3

--- send_to_s3.py
+++ send_to_s3.py
@@ -230,6 +230,28 @@ def delete_old_files(minutes=30, verbose=False):
                                                                                             time.localtime(max_time))))
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = file_name
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 def move_log_to_s3(log_copiado, logger):
     ahora = datetime.now()
     ayer = ahora - timedelta(days=1)
@@ -244,20 +266,16 @@ def move_log_to_s3(log_copiado, logger):
                             config=Config(signature_version='s3v4')
                             )
         bucket_name = 'proyecto-bd3-ff'
-        filename = '/logs/to_s3_' + ahora.strftime('%Y-%m-%d') +'.log'
-        print('Filename:'+filename)
-        try:
-            s3.client.upload_file('to_s3.log', bucket_name, filename)
-            # delete log
-            os.remove('to_s3.log')
-            # new logger
-            logger = setup_logger('to_s3_log', 'to_s3.log')
+        objectname = '/logs/to_s3_' + ahora.strftime('%Y-%m-%d') +'.log'
 
-            # actualizar hora de copia
-            log_copiado = ahora
+        upload_file('to_s3.log', bucket_name, objectname)
 
-        except ClientError as e:
-            logger.error(e)
+        # delete log
+        os.remove('to_s3.log')
+        # new logger
+        logger = setup_logger('to_s3_log', 'to_s3.log')
+        # actualizar hora de copia
+        log_copiado = ahora
 
     return log_copiado, logger
 
